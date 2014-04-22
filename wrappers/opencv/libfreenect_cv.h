0e4423d64645534d25bba9ba21e5b3d28ec869fe
nils <nils@tamu.edu>
2014-04-22 17:16:04 -0500

    Update libfreenect_cv, can choose 11BIT/REGISTERED

--- wrappers/opencv/libfreenect_cv.h
+++ wrappers/opencv/libfreenect_cv.h
@@ -4,10 +4,12 @@
 extern "C" {
 #endif
 
+#include "libfreenect.h"
+#include "libfreenect_sync.h"
 #include <opencv/cv.h>
 
-	IplImage *freenect_sync_get_depth_cv(int index);
-	IplImage *freenect_sync_get_rgb_cv(int index);
+	IplImage *freenect_sync_get_depth_cv(int index, freenect_depth_format fmt);
+	IplImage *freenect_sync_get_rgb_cv(int index, freenect_video_format fmt);
 
 #ifdef __cplusplus
 }
